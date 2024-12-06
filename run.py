548fb5999bb19583362c2f912f039766736a5c01
Developer <zhumabaevasadbek123@gmail.com>
2024-12-06 19:35:57 +0500

    reset

--- run.py
+++ run.py
@@ -1,14 +1,9 @@
 from app import bot
 import handlers
 
-def set_webhook():
-    import config
-    bot.remove_webhook()
-    bot.set_webhook(url=config.WEBHOOK_URL)
-
 def init():
     handlers.view_handle()
+    bot.polling()
 
 if __name__ == "__main__":
-    set_webhook()
     init()
