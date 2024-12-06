5bc86cbdd06964c6830895f8738c751a44db3b9d
Developer <zhumabaevasadbek123@gmail.com>
2024-12-06 19:07:56 +0500

    changes

--- config.py
+++ config.py
@@ -6,5 +6,8 @@ load_dotenv()
 # SECURITY WARNING: Keep the bot API token in production secret!
 BOT_API_TOKEN = os.getenv("BOT_API_TOKEN")
 
+# SECURITY WARNING: Keep the bot Webhook URL in production secret!
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+
 # Bot settings
 MESSAGE_CLEAR = eval(os.getenv("MESSAGE_CLEAR", "False"), {"__builtins__": None}) == True
--- run.py
+++ run.py
@@ -1,9 +1,16 @@
 from app import bot
 import handlers
 
+def set_webhook():
+    import config
+    webhook_url = config.WEBHOOK_URL
+    bot.remove_webhook()
+    bot.set_webhook(url=webhook_url)
+
 def init():
     handlers.view_handle()
     bot.polling()
 
 if __name__ == "__main__":
+    set_webhook()
     init()
