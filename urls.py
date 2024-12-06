4730ef46cc110fefbbe8b6928c6a7ae1b1215ced
Developer <zhumabaevasadbek123@gmail.com>
2024-12-06 19:07:58 +0500

    changes

--- urls.py
+++ urls.py
@@ -2,3 +2,4 @@ from app import app
 import views
 
 app.add_url_rule("/", "home", views.HomeView())
+app.add_url_rule("/webhook", "webhook", views.WebHookView())
--- views.py
+++ views.py
@@ -1,5 +1,11 @@
-from flask import render_template as render
+from flask import render_template as render, request
 
 class HomeView:
   def __call__(self):
     return render("leads/home.html")
+  
+class WebHook:
+  def __call__(self):
+    if request.headers.get('content-type') == 'application/json':
+        return '', 200
+    return '', 403
